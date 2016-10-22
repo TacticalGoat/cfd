f9d3d2c816d0a75106e9790ae869d3ad82bd3952
Arjun Narayan <arjun.narayan@gmail.com>
2016-10-22 22:15:33 +0000

    resolve merge

--- app/views.py
+++ app/views.py
@@ -3,6 +3,10 @@ from flask import request,redirect,url_for,current_app
 import unirest
 import urllib
 import httplib
+import duckduckgo
+import json
+import traceback
+from .sosearch import get_answers
 
 @app.route('/error/<error>')
 def error_handle(error):
@@ -23,13 +27,29 @@ def error_handle(error):
 	}
 	try:
 		print "Connecting"
-		conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-		conn.request("POST", "/text/analytics/v2.0/keyPhrases?%s" % params, str(body), headers)
-		response = conn.getresponse()
-		data = response.read()
-		print "DONE"
-		return data
+		links2 = duckduckgo.search(str(error),max_results=100)
+		links2 = list(set(links2))
+		print links2
+		stackoverflow = []
+
+		for link in links2:
+			if 'stackoverflow.com' in link:
+				stackoverflow.append(link)
+
+		sqids = []
+		for q in stackoverflow:
+			q_list = q.split('/')
+			if len(q_list) > 4:
+				sqids.append(q_list[4])
+		print sqids
+		answers = ""
+		for qid in sqids:
+			answer = get_answers(qid)
+			if answer is not None:
+				answers += answer
+		return answers
 	except Exception as e:
+		traceback.print_exc()
 		return str(e)
 
 @app.route('/error/',methods=['POST'])
@@ -43,11 +63,40 @@ def error_post():
 	body = str(request.form['body'])
 	data = None
 	try:
-		print "Connecting"
-		conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-		conn.request("POST", "/text/analytics/v2.0/keyPhrases?%s" % params, str(body), headers)
-		response = conn.getresponse()
-		data = response.read()
-		print "DONE"
+		#print "Connecting"
+		#conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+		#conn.request("POST", "/text/analytics/v2.0/keyPhrases?%s" % params, str(body), headers)
+		#response = conn.getresponse()
+		#data = response.read()
+		#print "DONE"
+		#print data
+		#result_dict = json.loads(data)
+		#key_phrase = str(result_dict['documents'][0]['keyPhrases'])
+		#links = duckduckgo.search(key_phrase,max_results=100)
+		links2 = duckduckgo.search(str(error),max_results=100)
+		links2 = list(set(links2))
+		#links = list(set(links))
+		#print links
+		print links2
+		print links2
+		stackoverflow = []
+
+		for link in links2:
+			if 'stackoverflow.com' in link:
+				stackoverflow.append(link)
+
+		sqids = []
+		for q in stackoverflow:
+			q_list = q.split('/')
+			if len(q_list) > 4:
+				sqids.append(q_list[4])
+		print sqids
+		answers = ""
+		for qid in sqids:
+			answer = get_answers(qid)
+			if answer is not None:
+				answers += answer
+		return answers
+		return 'success'
 	except Exception as e:
 		return str(e)
